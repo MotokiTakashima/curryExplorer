e9434f22f8dc89c2aefd28b3883ebcfeeff2c3f6
MotokiTakashima <pageboy.jr@icloud.com>
2024-01-27 14:45:11 +0900

    店舗テーブル関連を追加

---
+++ app/Http/Controllers/ShopsController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreShopsRequest;
+use App\Http\Requests\UpdateShopsRequest;
+use App\Models\Shops;
+
+class ShopsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreShopsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Shops $shops)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Shops $shops)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateShopsRequest $request, Shops $shops)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Shops $shops)
+    {
+        //
+    }
+}
---
+++ app/Models/Shops.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Shops extends Model
+{
+    use HasFactory;
+    protected $table = 'shops';
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'phone',
+        'website',
+        'description',
+        'rating',
+    ];
+}
